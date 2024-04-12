d00ddea6a482b662174b25672d206cd1de481321
Juan David <jdmm360@gmail.com>
2024-04-11 22:21:46 -0500

    feat: dockerize the API

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { createNewCourse } from "./src/controllers/services.controllers.js";
 import { connectToDatabase } from "./db.js";
 
 
-import routes from "./src/routes/index.js"
-
-
-
+import routes from "./src/routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-connectToDatabase()
+connectToDatabase();
 
+app.use("/api/v1", routes);
 
+const port = process.env.PORT ?? 3000;
 
-app.use("/api/v1",routes)
-
-
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
 });
