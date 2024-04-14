f4fdb488ca7bfd7a15926c94a085c16191cef36c
Dylanyessid <dylanyessid1904@gmail.com>
2024-04-14 14:16:00 -0500

    chore: config.js created

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 import { createRequire } from "module";
 import { CoursesSchema } from "./src/models/Courses.js";
 import { AdminsSchema } from "./src/models/admin.js";
-import dotenv from "dotenv";
+
+import configs from "./src/config/config.js";
 const require = createRequire(import.meta.url);
 const typeorm = require("typeorm");
-dotenv.config();
+
 export const dataSource = new typeorm.DataSource({
   type: "mysql",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: configs.database.host,
+  port:configs.database.port,
+  username: configs.database.username,
+  password: configs.database.password,
+  database: configs.database.database,
   //   synchronize: true,
   entities: [CoursesSchema, AdminsSchema],
 });
