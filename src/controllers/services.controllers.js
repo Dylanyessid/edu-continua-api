5ca841f7429a85cfb8f71f5dd72ca151a80c2a26
Dylanyessid <dylanyessid1904@gmail.com>
2024-06-23 20:34:51 -0500

    fix: fix merge error

--- src/controllers/services.controllers.js
+++ src/controllers/services.controllers.js
@@ -1,4 +1,3 @@
-
 import { dataSource } from "../../db.js";
 
 import { FilterParser } from "../helpers/filterParser.js";
@@ -7,26 +6,29 @@ import { uploadFile } from "../services/cloudinary.js";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const {IsNull, Equal} = require("typeorm");
+const { IsNull, Equal } = require("typeorm");
 
-const formationServiceRepository = dataSource.getRepository("FormationServices");
-
+const formationServiceRepository =
+  dataSource.getRepository("FormationServices");
 
 export const createNewFormationService = async (req, res) => {
-
   const { type } = req.body;
 
-  if(!type || type && !isValidType(type)){
+  if (!type || (type && !isValidType(type))) {
     return res
-    .status(400)
-    .json({ isSuccess: false, message: "Please provide a valid type of course: 'course','seminar', 'workshop', 'diploma'" });
+      .status(400)
+      .json({
+        isSuccess: false,
+        message:
+          "Please provide a valid type of course: 'course','seminar', 'workshop', 'diploma'",
+      });
   }
-  
+
   try {
     let data = {
       name: req.body.name,
       generalInfo: req.body.generalInfo,
-      type:req.body.type,
+      type: req.body.type,
       image: req.body.image,
       syllabus: req.body.syllabus,
       hours: req.body.hours,
@@ -54,139 +56,141 @@ export const createNewFormationService = async (req, res) => {
   }
 };
 
+export const getFormationServicesByPagination = async (req, res) => {
+  const { skip, taking, filters } = req.query;
+  try {
+    const { skip, taking, filters } = req.query;
 
-
-
-export const getFormationServicesByPagination = async (req,res)=>{
-
-
-  try{
-    if(!req.query.skip || !req.query.taking || isNaN(Number(req.query.skip)) || isNaN(Number(req.query.taking))){
-      return res
-      .status(400)
-      .json({ isSuccess: false, message: "Please provide correct values for pagination. Example: /10/0. This will take 10 elements of each service, starting of id 1. You will get 40 total objects" });
-    }
     let where = { deletedAt: IsNull() };
     if (filters) {
       const parser = new FilterParser();
       where = { ...where, ...parser.filterParser(filters, ["name", "type", "generalInfo"]) };
-    const {skip, taking} = req.query
-   
+    }
+  
     const [services,count] =  await formationServiceRepository.findAndCount({
-      where:{
-        deletedAt:IsNull()
-      },
-      order:{
-        id:"DESC"
-      },
-      take:taking,
+      where,
+      order: { id: "DESC" },
+      take: taking,
       skip
-    })
-
+    });
 
     return res
     .status(200)
     .json({ isSuccess: true, message: "ok", data:services,count });
-
-  }catch(error){
-
-    return res
-    .status(500)
-    .json({ isSuccess: false, message: "Ha ocurrido un error." });
-  }
-}
-
-export const getSingleFormationService = async (req,res)=>{
-
-  try{
-    if(isNaN(Number(req.params.id))){
-      return res
-      .status(400)
-      .json({ isSuccess: false, message: "Please provide a correct value for id. Example: 5. The id must be numeric" });0
-    }
-
-   
-    const formationService =  await formationServiceRepository.findOne({where:{id:Equal(Number(req.params.id))}})
-
-    return res
-    .status(200)
-    .json({ isSuccess: true, message: "ok", data:formationService });
-  }catch(error){
-    return res
-    .status(500)
-    .json({ isSuccess: false, message: "Ha ocurrido un error." });
-  }
-}
-
-
-export const updateFormationService = async (req,res)=>{
-  if(isNaN(Number(req.params.id))){
-    return res
-    .status(400)
-    .json({ isSuccess: false, message: "Please provide correct values for id. Id must be a number" });
-  }
-  const type = req.body.type
-  if(!type || type && !isValidType(type)){
-    return res
-    .status(400)
-    .json({ isSuccess: false, message: "Please provide a valid type of course: 'course','seminar', 'workshop', 'diploma'" });
-  }
- 
-  try {
-    const serviceRecord = await formationServiceRepository.findOne({where:{id:Number(req.params.id)}})
-    if(!serviceRecord){
-      return res
-      .status(400)
-      .json({ isSuccess: false, message: "Formation Service not found" });
-    }
-
-    let dataToUpdate = {
-      ...req.body
-    }
-    for (const imageField of Object.keys(req.files)) {
-      let file = req.files[imageField][0];
-      const res = await uploadFile(file);
-      dataToUpdate[imageField] = res.secure_url;
-    }
-    Object.assign(serviceRecord,dataToUpdate)
-    await formationServiceRepository.update({id:Number(serviceRecord.id)},serviceRecord)
-    
-    return res
-    .status(204)
-
-    .json({ isSuccess: true, message: "Service updated" });
   } catch (error) {
     return res
       .status(500)
       .json({ isSuccess: false, message: "Ha ocurrido un error." });
   }
-}
+};
 
-
-export const deleteFormationService = async (req,res)=>{
-  if(isNaN(Number(req.params.id))){
-    return res
-    .status(400)
-    .json({ isSuccess: false, message: "Please provide a correct value for id. Id must be a number" });
-  }
-
-
-  
+export const getSingleFormationService = async (req, res) => {
   try {
-    const serviceRecord = await formationServiceRepository.findOne({where:{id:Number(req.params.id)}})
-    if(!serviceRecord){
+    if (isNaN(Number(req.params.id))) {
       return res
-      .status(400)
-      .json({ isSuccess: false, message: "Formation Service not found" });
+        .status(400)
+        .json({
+          isSuccess: false,
+          message:
+            "Please provide a correct value for id. Example: 5. The id must be numeric",
+        });
+      0;
     }
-    serviceRecord.deletedAt = new Date()
-    await formationServiceRepository.save(serviceRecord)
+
+    const formationService = await formationServiceRepository.findOne({
+      where: { id: Equal(Number(req.params.id)) },
+    });
+
     return res
-    .status(200)
-    .json({ isSuccess: true, message: "Deleted" });
+      .status(200)
+      .json({ isSuccess: true, message: "ok", data: formationService });
   } catch (error) {
     return res
-    .status(500)
-    .json({ isSuccess: false, message: "Ha ocurrido un error." });
+      .status(500)
+      .json({ isSuccess: false, message: "Ha ocurrido un error." });
   }
-}
+};
+
+export const updateFormationService = async (req, res) => {
+  if (isNaN(Number(req.params.id))) {
+    return res
+      .status(400)
+      .json({
+        isSuccess: false,
+        message: "Please provide correct values for id. Id must be a number",
+      });
+  }
+  const type = req.body.type;
+  if (!type || (type && !isValidType(type))) {
+    return res
+      .status(400)
+      .json({
+        isSuccess: false,
+        message:
+          "Please provide a valid type of course: 'course','seminar', 'workshop', 'diploma'",
+      });
+  }
+
+  try {
+    const serviceRecord = await formationServiceRepository.findOne({
+      where: { id: Number(req.params.id) },
+    });
+    if (!serviceRecord) {
+      return res
+        .status(400)
+        .json({ isSuccess: false, message: "Formation Service not found" });
+    }
+
+    let dataToUpdate = {
+      ...req.body,
+    };
+    for (const imageField of Object.keys(req.files)) {
+      let file = req.files[imageField][0];
+      const res = await uploadFile(file);
+      dataToUpdate[imageField] = res.secure_url;
+    }
+    Object.assign(serviceRecord, dataToUpdate);
+    await formationServiceRepository.update(
+      { id: Number(serviceRecord.id) },
+      serviceRecord
+    );
+
+    return res
+      .status(204)
+
+      .json({ isSuccess: true, message: "Service updated" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ isSuccess: false, message: "Ha ocurrido un error." });
+  }
+};
+
+export const deleteFormationService = async (req, res) => {
+  if (isNaN(Number(req.params.id))) {
+    return res
+      .status(400)
+      .json({
+        isSuccess: false,
+        message: "Please provide a correct value for id. Id must be a number",
+      });
+  }
+
+  try {
+    const serviceRecord = await formationServiceRepository.findOne({
+      where: { id: Number(req.params.id) },
+    });
+    if (!serviceRecord) {
+      return res
+        .status(400)
+        .json({ isSuccess: false, message: "Formation Service not found" });
+    }
+    serviceRecord.deletedAt = new Date();
+    await formationServiceRepository.save(serviceRecord);
+    return res.status(200).json({ isSuccess: true, message: "Deleted" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ isSuccess: false, message: "Ha ocurrido un error." });
+  }
+};
